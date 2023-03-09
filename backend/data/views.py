ec04cf4a6c63f609761ef08823f8cd25d304137e
StEugen <85479518+StEugen@users.noreply.github.com>
2023-03-09 12:14:42 +0000

    working on views

--- backend/data/views.py
+++ backend/data/views.py
@@ -1,2 +1,19 @@
 from django.shortcuts import render
+import csv 
+from django.http import JsonResponse
+
+def extract_data_csv(filename):
+    data = []
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append({
+                'id': row[id]
+            })
+    return data
+
+
+def data_view(request):
+    data = extract_data_csv('./csvFiles/data-structure.csv')
+    return JsonResponse(data, safe=False)
 
