ebd0706876c31dde681f47a83383fd2495231ae5
StEugen <85479518+StEugen@users.noreply.github.com>
2023-03-10 13:03:23 +0000

    Update views.py
    api route is working correctly

--- backend/data/views.py
+++ backend/data/views.py
@@ -16,12 +16,37 @@ class DataViewset(viewsets.ViewSet):
 def extract_data_csv(filename):
     data = []
     with open(filename, newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
+        reader = csv.reader(csvfile)
+        header = next(reader)
+        id_col = header.index('id:')
+        name_col = header.index('Название:')
+        name_eng = header.index('Название (in english):')
+        active = header.index('Активный:')
+        description = header.index('Краткое описание:')
+        long_description = header.index('Детальное описание:')
+        description_eng = header.index(
+            'Краткое описание (in english):'
+            )
+        long_description_eng = header.index(
+            'Детальное описание (in english):'
+            )
+        mo = header.index('МО:')
+        federal_subject = header.index('Субъект федерации:')
+        significance = header.index('Значимость:')
+        place = header.index('Населенный пункт:')
+        place_eng = header.index('Населенный пункт (in english):')
+        Address = header.index('Адрес:')
+        Address_eng = header.index('Адрес (in english):')
+
         for row in reader:
+            id_value = row[id_col]
+            name_value = row[name_col]
             data.append({
-                "id": row["id"],
+                "id": id_value,
+                "name": name_value,
+
             })
-    return 
+    return data
 
 
 def upload_file(file):
