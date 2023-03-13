e541dafe9dc35ff2e1ea66b65fe66895c1607c60
StEugen <85479518+StEugen@users.noreply.github.com>
2023-03-13 20:03:21 +0000

    Created new test

--- backend/data/tests.py
+++ backend/data/tests.py
@@ -1,35 +1,35 @@
+from django.test import TestCase, Client
 from django.urls import reverse
-from rest_framework.test import APITestCase
-from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-class AccountsTest(APITestCase):
+class CreateUserViewTestCase(TestCase):
     def setUp(self):
-        self.test_user = User.objects.create_user(
-            'testuser', 
-            'test@example.com', 
-            'testpassword'
-        )
-
-        self.create_url = reverse('account-create')
-
-    def test_create_user(self):
-        """
-        Ensure we can create a new user and a valid token is created with it.
-        """
-        data = {
-            'username': 'foobar',
-            'email': 'foobar@example.com',
-            'password': 'somepassword'
+        self.client = APIClient()
+        self.register_url = reverse('register')
+        self.user_data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+            'email': 'test@example.com'
         }
 
-        response = self.client.post(self.create_url , data, format='json')
-
-
-        self.assertEqual(User.objects.count(), 2)
+    def test_user_with_valid_data(self):
+        response = self.client.post(self.register_url, data=self.user_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['username'], data['username'])
-        self.assertEqual(response.data['email'], data['email'])
-        self.assertFalse('password' in response.data)
+        user = User.objects.get(username=self.user_data['username'])
+        self.assertEqual(user.email, self.user_data['email'])
+        serializer = UserSerializer(instance=user)
+        self.assertEqual(response.data, serializer.data)
 
+    def test_user_with_missing_data(self):
+        invalid_data = {k:v for k,v in self.user_data.items() if k != 'email'}
+        response = self.client.post(self.register_url, data=invalid_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_with_existing_username(self):
+        existing_user = User.objects.create_user(**self.user_data)
+        response = self.client.post(self.register_url, data=self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
--- backend/data/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import views
-
-urlpatterns = [
-    re_path(r'api/users^$', views.UserCreate.as_view(), name='account-create'),
-]
